b84cd7f76acff1d67315ebb872a4bfeae360fdd5
Partab Rai <amrani.partab@gmail.com>
2023-02-17 21:25:34 +0500

    changes

--- controller/openai.js
+++ controller/openai.js
@@ -1,12 +1,11 @@
 import { Configuration, OpenAIApi } from "openai";
 import config from "config";
 
-const configuration = new Configuration({
-  apiKey: config.get("OPENAI_SECRET"),
-});
-const openai = new OpenAIApi(configuration);
-
 export const createProduct = async (req, res) => {
+  const configuration = new Configuration({
+    apiKey: config.get("OPENAI_SECRET"),
+  });
+  const openai = new OpenAIApi(configuration);
   const { number, theme, ingredients, dish } = req.body;
   const imageSize = `512x512`;
   const textPrompt = `Create a catchy description for the following restauraunt or foods based on the folling details. ingredients: ${ingredients} , theme: ${theme}, dish : ${dish}:`;
@@ -20,7 +19,7 @@ export const createProduct = async (req, res) => {
 
     const description = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt:textPrompt,
+      prompt: textPrompt,
       temperature: 0.8,
       max_tokens: 60,
       top_p: 1,
@@ -30,18 +29,18 @@ export const createProduct = async (req, res) => {
 
     const imageUrl = response;
     const data = {
-      images:imageUrl.data.data,
-      description:description.data.choices[0].text,
-    }
+      images: imageUrl.data.data,
+      description: description.data.choices[0].text,
+    };
 
     res.status(200).json({
       success: true,
-      data:data,
+      data: data,
     });
   } catch (error) {
     if (error) {
       console.log(error);
-    } 
+    }
 
     res.status(400).json({
       success: false,

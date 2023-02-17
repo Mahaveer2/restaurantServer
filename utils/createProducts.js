849e6e7928a5012ef0c905b86601e0cf3fd6aef0
Partab Rai <amrani.partab@gmail.com>
2023-02-17 15:16:36 +0500

    changes react

--- utils/createProducts.js
+++ utils/createProducts.js
@@ -4,7 +4,6 @@ export const createProducts = () => {
   productSchema.deleteMany({},() => {})
   productSchema.countDocuments({}).exec((err,count) => {
     if(count < 3){
-      productSchema.deleteMany(() => {})
       productSchema.create([
         {
           id:"price_1McDIDFbuSFlLnppYnVeZAr3",
@@ -30,7 +29,29 @@ export const createProducts = () => {
       ]);
     }else if(count > 3){
       productSchema.deleteMany({},() => {});
-      createProducts();
+      productSchema.create([
+        {
+          id:"price_1McDIDFbuSFlLnppYnVeZAr3",
+          name:"Starter",
+          amount:"15",
+          type:"monthly",
+          description:"Create a new subscription",
+        },
+        {
+          id:"price_1McDJ2FbuSFlLnppcsWd4dN0",
+          name:"Advanced",
+          amount:"30",
+          type:"monthly",
+          description:"Advanced subscription package",
+        },
+        {
+          id:"price_1McDJYFbuSFlLnpp7nNQQQDO",
+          name:"Premium",
+          amount:"50",
+          type:"monthly",
+          description:"Premium subscription package",
+        }
+      ]);
     }
   })
   
